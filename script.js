cbdde097d74d858adf34dcc6fa341d8135d09e9d
Mayukh Dantuluri <mayukh.dantuluri@outlook.com>
2026-09-21 12:11:38 -0400

    Fix detail formatting for Biology pre-lab task

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const ANNOUNCEMENTS = [
     },
     {
         title: "Adv. Science 4: Biology",
-        detail: "- Finish the Calorimetry Pre-Lab Questions on a document on your Google Drive, have it ready by 2026-09-22"
+        detail: "- Finish the Calorimetry Pre-Lab Questions on a document on your Google Drive, have it ready by 2026-09-22."
     },
     {
         title: "Algorithms & Data Structures",
